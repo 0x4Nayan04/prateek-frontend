84d360e1d7884419154e0fc2926f79d3bc8a8214
Nayan <nayan_s@ce.iitr.ac.in>
2025-06-19 22:47:06 +0530

    Update role and location in content.js; change baseURL in once-ui.config.js for deployment to Vercel.

--- src/resources/content.js
+++ src/resources/content.js
@@ -4,10 +4,10 @@ const person = {
 	get name() {
 		return `${this.firstName} ${this.lastName}`;
 	},
-	role: 'Software Engineer',
+	role: 'Product Manager',
 	avatar: '/images/avatar.jpg',
 	email: 'pratik@pratik.com',
-	location: 'Asia/Jakarta', // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+	location: 'India', // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
 	languages: ['English', 'Bahasa'] // optional: Leave the array empty if you don't want to display languages
 };
 
